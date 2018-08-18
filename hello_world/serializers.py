bef74c264765a88787b32cc2b409168c986a1259
Kalenshi Katebe <math-matician@localhost.localdomain>
2018-08-18 10:52:47 -0300

    Only validation remaining

--- hello_world/serializers.py
+++ hello_world/serializers.py
@@ -2,5 +2,5 @@ from rest_framework import serializers
 from hello_world.models import Iata
 
 class InputSerializer(serializers.Serializer):
-    iata_code_1 = serializers.CharField(max_length=3,style={"placeholder":"Enter Three Characters,eg XXX","max_length":"3"})
-    iata_code_2 = serializers.CharField(max_length=3,style={"placeholder":"Enter Three Characters,eg XXX","max_length":"3"})
+    iata_code_1 = serializers.CharField(label="IATA Code 1",max_length=3,style={"placeholder":"Enter Three Characters,eg XXX","max_length":"3"})
+    iata_code_2 = serializers.CharField(label="IATA Code 2",max_length=3,style={"placeholder":"Enter Three Characters,eg XXX","max_length":"3"})
--- hello_world/views.py
+++ hello_world/views.py
@@ -8,6 +8,7 @@ from hello_world.models import Iata
 from hello_world  import serializers
 
 
+
 # Create your views here.
 class HelloView(APIView):
     #
@@ -16,23 +17,49 @@ class HelloView(APIView):
     def get(self,request,format=None):
         message = {
         "name": "Distance Calculator",
-        "input 1": "IATA code for origin",
-        "Input 2": "IATA code for destination",
+        "input 1": "IATA code for the origin airport",
+        "Input 2": "IATA code for the destination airport",
         "Output":{
-            "status": "Indicating success or failure",
-            "code 1": "The origin IATA code",
-            "code 2": "The destination IATA code",
-            "distance": "The distance between the two points on the globe"
+            "Success status": {
+                    "0":"Success",
+                    "1":"One of the location IATA code is not in the database",
+                    "2":"Invalid Input"},
+            "IATA Code 1": "The origin IATA code",
+            "IATA Code 2": "The destination IATA code",
+            "distance": "The distance between the two airports on the globe"
             }
         }
         return Response(message)
 
     def post(self,request,format=None):
+        import math
         code_1=None
         code_2=None
         serializer = serializers.InputSerializer(data=request.data)
         if serializer.is_valid():
             code_1 = serializer.data.get('iata_code_1')
             code_2 = serializer.data.get('iata_code_2')
+            try:
+                lat1=float(Iata.objects.get(pk=code_1).lat)
+                lng1=float(Iata.objects.get(pk=code_1).lng)
+                lat2=float(Iata.objects.get(pk=code_2).lat)
+                lng2=float(Iata.objects.get(pk=code_2).lng)
 
-        return Response({"CODE 1":code_1,"CODE 2":code_2})
+                delta_lat = math.radians(lat2-lat1)
+                delta_lng = math.radians(lng2-lng1)
+
+                lat1=math.radians(lat1)
+                lat2=math.radians(lat2)
+
+                RADIUS = 6371
+                haversine = (math.sin(delta_lat/2))**2 + math.cos(lat1)*math.cos(lat2)*(math.sin(delta_lng/2))**2 #works well up to this point
+                mul  = 2 * math.atan2(math.sqrt(haversine),math.sqrt(1-haversine))
+                distance = RADIUS * mul
+                return Response({"Status":"0","IATA Code 1":code_1,"IATA Code 2":code_2,"Distance":distance})
+
+            except:
+                return Response({"Status":"1","CODE 1":code_1,"CODE 2":code_2})
+
+        code_1 = serializer.data.get('iata_code_1')
+        code_2 = serializer.data.get('iata_code_2')
+        return Response({"Status":"2","CODE 1":code_1,"CODE 2":code_2})
