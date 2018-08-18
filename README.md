5919e69c4190a4c3d79f4ae667b9f4ee3c124209
Kalenshi Katebe <math-matician@localhost.localdomain>
2018-08-18 12:05:05 -0300

    final submission

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Django REST API used to determine the distance between two locations  using the
 locations' IATA codes.
 The codes are compared with those in the database,used to retrieve the latitude
 and longitude  which are then used to compute the distance between the locations
+
+To begin run the 'populate_iata.py' script to ensure that the coordinates are in the database
--- hello_world/serializers.py
+++ hello_world/serializers.py
@@ -1,6 +1,25 @@
 from rest_framework import serializers
 from hello_world.models import Iata
+import re
+
+def validate_input(value):
+    value = re.sub(r'[^A-Z,a-z]','',value)
+    if len(value) != 3:
+        raise serializers.ValidationError("Invalid Input")
+    return value
 
 class InputSerializer(serializers.Serializer):
-    iata_code_1 = serializers.CharField(label="IATA Code 1",max_length=3,style={"placeholder":"Enter Three Characters,eg XXX","max_length":"3"})
-    iata_code_2 = serializers.CharField(label="IATA Code 2",max_length=3,style={"placeholder":"Enter Three Characters,eg XXX","max_length":"3"})
+    iata_code_1 = serializers.CharField(
+                label="IATA Code 1",validators=[validate_input,],
+                max_length=3,
+                style={
+                    "placeholder":"Enter Three Characters,eg XXX",
+                    "max_length":"3"
+                    })
+    iata_code_2 = serializers.CharField(
+                label="IATA Code 2",validators=[validate_input,],
+                max_length=3,
+                style={
+                    "placeholder":"Enter Three Characters,eg XXX",
+                    "max_length":"3"
+                    })
